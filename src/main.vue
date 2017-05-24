de1d72cf97feeaad6dd7fabb3777944f6a6a94cd
Greg Wolff <gregwwolff@gmail.com>
2017-05-23 23:17:25 -0700

    fixedmap

--- src/main.vue
+++ src/main.vue
@@ -1,47 +1,47 @@
 <template>
-	<!-- App -->
-	<div id="app">
+<!-- App -->
+<div id="app">
 
-		<!-- Statusbar -->
-		<f7-statusbar></f7-statusbar>
+  <!-- Statusbar -->
+  <f7-statusbar></f7-statusbar>
 
-		<!-- Right Panel -->
-		<f7-panel right cover layout="dark">
-			<f7-view id="right-panel-view" navbar-through :dynamic-navbar="true">
-				<f7-navbar title="Right Panel" sliding></f7-navbar>
-				<f7-pages>
-					<f7-page>
-						<f7-block>
-							<p>Right panel content goes here</p>
-						</f7-block>
-						<f7-block-title>Load page in panel</f7-block-title>
-						<f7-list>
-							<f7-list-item link="/form/" title="Form"></f7-list-item>
-						</f7-list>
-						<f7-block-title>Load page in main view</f7-block-title>
-						<f7-list>
-							<f7-list-item link="/form/" title="Form" link-view="#main-view" link-close-panel></f7-list-item>
-						</f7-list>
-						<f7-list>
-							<f7-list-item link="/map/" title="Map" link-view="#main-view" link-close-panel></f7-list-item>
-						</f7-list>
-					</f7-page>
-				</f7-pages>
-			</f7-view>
-		</f7-panel>
+  <!-- Right Panel -->
+  <f7-panel right cover layout="dark">
+    <f7-view id="right-panel-view" navbar-through :dynamic-navbar="true">
+      <f7-navbar title="Right Panel" sliding></f7-navbar>
+      <f7-pages>
+        <f7-page>
+          <f7-block>
+            <p>Right panel content goes here</p>
+          </f7-block>
+          <f7-block-title>Load page in panel</f7-block-title>
+          <f7-list>
+            <f7-list-item link="/form/" title="Form"></f7-list-item>
+          </f7-list>
+          <f7-block-title>Load page in main view</f7-block-title>
+          <f7-list>
+            <f7-list-item link="/form/" title="Form" link-view="#main-view" link-close-panel></f7-list-item>
+          </f7-list>
+          <f7-list>
+            <f7-list-item link="/map/" title="Map" link-view="#main-view" link-close-panel></f7-list-item>
+          </f7-list>
+        </f7-page>
+      </f7-pages>
+    </f7-view>
+  </f7-panel>
 
-		<!-- Main Views -->
-		<f7-views>
-			<f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
-				<!-- Navbar -->
-				<f7-navbar>
-					<f7-nav-center sliding>Crimewatch</f7-nav-center>
-					<f7-nav-right>
-						<f7-link icon="icon-bars" open-panel="right"></f7-link>
-					</f7-nav-right>
-				</f7-navbar>
-				<!-- Pages -->
-				<!--
+  <!-- Main Views -->
+  <f7-views>
+    <f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
+      <!-- Navbar -->
+      <f7-navbar>
+        <f7-nav-center sliding>Crimewatch</f7-nav-center>
+        <f7-nav-right>
+          <f7-link icon="icon-bars" open-panel="right"></f7-link>
+        </f7-nav-right>
+      </f7-navbar>
+      <!-- Pages -->
+      <!--
 				<f7-pages>
 					<f7-page>
 						<f7-block-title>Welcome to my App</f7-block-title>
@@ -51,27 +51,25 @@
 					</f7-page>
 				</f7-pages>
 				-->
-				<f7-pages>
-					<f7-page>
-						<GmapMap
-						:center="center"
-						:zoom="7"
-						style="width: 100%; height:100%"
-						@drag="resize()"
-						></GmapMap>
-					</f7-page>
-				</f7-pages>
-			</f7-view>
-		</f7-views>
-	</div>
+      <f7-pages>
+        <f7-page>
+          <GmapMap :center="center" :zoom="7" style="width: 100%; height:100%"></GmapMap>
+        </f7-page>
+      </f7-pages>
+    </f7-view>
+  </f7-views>
+</div>
 </template>
 
 <script>
 export default {
-	data () {
-		return {
-			center: {lat: 10.0, lng: 10.0},
-		}
-	}
+  data() {
+    return {
+      center: {
+        lat: 10.0,
+        lng: 10.0
+      },
+    }
+  }
 }
 </script>
